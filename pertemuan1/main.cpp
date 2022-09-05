af58d1fab82765df6a5e3cb2bffe09227ea6bbb7
irfaneka1 <112864817+irfaneka1@users.noreply.github.com>
2022-09-05 18:08:36 +0700

    Add files via upload

---
+++ pertemuan1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Muhammad Irfan Eka Dhamhudi" << endl;
+    cout << "NIM : A11.2022.14136" << endl;
+    cout << "Alamat : Ds.Natai Baru Kalimantan Tengah" << endl;
+    cout << "Asal Sekolah : SMKN 1 Pangkalanbun" << endl;
+    return 0;
+}
