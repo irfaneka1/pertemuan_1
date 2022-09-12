b4d4d8cba8d9eac5daffb80be49c85fe9995d85a
irfaneka1 <112864817+irfaneka1@users.noreply.github.com>
2022-09-12 22:18:18 +0700

    Add files via upload

---
+++ pertemuan 3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "=======================================" << endl;
+    cout << "Nama : Muhammad Irfan Eka D" << endl;
+    cout << "Nim : A11.2022.14136" << endl;
+
+    int a = 19;
+    cout << "umur saya = " << a;
+    float t = 169;
+    cout << "\ntinggi saya = " << t;
+    char x = 'L';
+    cout << "\nukuran baju saya = " << x;
+    string l = "XXL";
+    cout << "\nukuran baju teman saya = " << l;
+    cout << "\n=======================================" << endl;
+
+    return 0;
+}
